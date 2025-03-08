c377aec7f7b71c91dcc239c79c612c6762e28002
doodlum <doodlum@users.noreply.github.com>
2025-03-08 16:16:04 +0000

    style: 🎨 apply clang-format changes

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2301,7 +2301,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace
 		skylightingDiffuse = lerp(1.0, skylightingDiffuse, skylightingFadeOutFactor);
 
 		skylightingDiffuse = Skylighting::mixDiffuse(SharedData::skylightingSettings, skylightingDiffuse);
-		
+
 		directionalAmbientColor = Color::GammaToLinear(directionalAmbientColor);
 
 		directionalAmbientColor *= skylightingDiffuse;
