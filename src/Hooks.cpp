1ef17262ff75f855ac731e9ee3dd168374ef7d6b
doodlum <doodlum@users.noreply.github.com>
2025-03-13 15:48:05 +0000

    style: 🎨 apply clang-format changes

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -347,7 +347,7 @@ HRESULT WINAPI hk_D3D11CreateDeviceAndSwapChain(
 	upscaling->lowRefreshRate = refreshRate < 119;
 	upscaling->isWindowed = pSwapChainDesc->Windowed;
 
-	const D3D_FEATURE_LEVEL featureLevels[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_11_1};
+	const D3D_FEATURE_LEVEL featureLevels[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_11_1 };
 
 	if (shouldProxy) {
 		logger::info("[Frame Generation] Frame Generation enabled, using D3D12 proxy");
