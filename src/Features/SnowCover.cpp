37d550b2497fb8cfb58960415ed9cdad652b8150
ThePagi <ThePagi@users.noreply.github.com>
2025-05-05 07:30:07 +0000

    style: 🎨 apply clang-format changes

--- src/Features/SnowCover.cpp
+++ src/Features/SnowCover.cpp
@@ -182,8 +182,7 @@ SnowCover::PerFrame SnowCover::GetCommonBufferData()
 				}
 				if (currentWeather->data.flags.any(RE::TESWeather::WeatherDataFlag::kSnow)) {
 					snowing = true;
-				}
-				else if (currentWeather->data.flags.any(RE::TESWeather::WeatherDataFlag::kRainy)) {
+				} else if (currentWeather->data.flags.any(RE::TESWeather::WeatherDataFlag::kRainy)) {
 					raining = true;
 				}
 			}
