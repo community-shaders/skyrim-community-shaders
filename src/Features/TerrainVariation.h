94044fd418f664af7da3192b847bc5f52e2ea1db
David Kehoe <davidkehoe0411@outlook.com>
2025-05-06 21:42:09 +1000

    adding haka's nexus download stuff

--- src/Features/TerrainVariation.h
+++ src/Features/TerrainVariation.h
@@ -10,6 +10,7 @@ struct TerrainVariation : Feature
 
 	virtual inline std::string GetName() override { return "Terrain Variation"; }
 	virtual inline std::string GetShortName() override { return "TerrainVariation"; }
+	virtual inline std::string GetFeatureModLink() override { return "https://www.nexusmods.com/skyrimspecialedition/mods/148123"; }
 	virtual inline std::string_view GetShaderDefineName() override { return "TERRAIN_VARIATION"; }
 	virtual inline bool HasShaderDefine(RE::BSShader::Type shaderType) override { return shaderType == RE::BSShader::Type::Lighting; }
 	virtual bool IsCore() const override { return false; };
@@ -46,4 +47,5 @@ struct TerrainVariation : Feature
 
 	virtual void PostPostLoad() override;
 	void UpdateShaderSettings();
+	virtual void DrawUnloadedUI() override;
 };
