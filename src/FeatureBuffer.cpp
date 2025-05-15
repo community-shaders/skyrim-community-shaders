a20be44fb0bc34f1cc9dcfbf833011e992d8df8f
David Kehoe <davidkehoe0411@outlook.com>
2025-05-16 09:28:53 +1000

    fix

--- src/FeatureBuffer.cpp
+++ src/FeatureBuffer.cpp
@@ -42,6 +42,6 @@ std::pair<unsigned char*, size_t> GetFeatureBufferData(bool a_inWorld)
 		globals::features::skylighting->GetCommonBufferData(a_inWorld),
 		globals::features::cloudShadows->settings,
 		globals::features::lodBlending->settings,
+		globals::features::hairSpecular->settings,
 		globals::features::terrainVariation->settings);
-		globals::features::hairSpecular->settings);
 }
