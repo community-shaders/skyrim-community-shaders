ce33dbb1d8c0c9b1e9ce96d4702a6b1f43eb16da
Pentalimbed <Pentalimbed@users.noreply.github.com>
2025-05-19 03:37:29 +0000

    style: 🎨 apply clang-format changes

--- src/Features/TerrainVariation.cpp
+++ src/Features/TerrainVariation.cpp
@@ -1,9 +1,8 @@
+#include "TerrainVariation.h"
 #include "../FeatureBuffer.h"
 #include "../Globals.h"
 #include "../State.h"
 #include "../Util.h"
-#include "TerrainVariation.h"
-
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(
 	TerrainVariation::Settings,
