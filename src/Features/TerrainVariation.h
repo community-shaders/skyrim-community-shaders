bf13dbb6a808002fd8791fa402ad267f7bc6a98a
David Kehoe <davidkehoe0411@outlook.com>
2025-05-25 20:25:54 +1000

    Update TerrainVariation.h

--- src/Features/TerrainVariation.h
+++ src/Features/TerrainVariation.h
@@ -7,12 +7,12 @@ struct TerrainVariation : Feature
 		static TerrainVariation singleton;
 		return &singleton;
 	}
-
 	virtual inline std::string GetName() override { return "Terrain Variation"; }
 	virtual inline std::string GetShortName() override { return "TerrainVariation"; }
 	virtual inline std::string GetFeatureModLink() override { return "https://www.nexusmods.com/skyrimspecialedition/mods/148123"; }
-	virtual inline std::string_view GetShaderDefineName() override { return "TERRAIN_VARIATION"; }
-	virtual inline bool HasShaderDefine(RE::BSShader::Type shaderType) override { return shaderType == RE::BSShader::Type::Lighting; }
+	virtual inline std::string_view GetShaderDefineName() override { return "TERRAIN_VARIATION"; }	virtual inline bool HasShaderDefine(RE::BSShader::Type shaderType) override { 
+		return (shaderType == RE::BSShader::Type::Lighting); 
+	}
 	virtual bool IsCore() const override { return false; };
 	virtual bool SupportsVR() override { return true; }
 	struct Settings
