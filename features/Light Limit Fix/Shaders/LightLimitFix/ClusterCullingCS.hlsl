811a77088846299cf7997e4c06f73e1627250aef
soda3000 <toonlink2020@hotmail.com>
2025-06-07 17:34:38 -0300

    WIP: Custom Menu and UI changes. Also fk github

--- features/Light Limit Fix/Shaders/LightLimitFix/ClusterCullingCS.hlsl
+++ features/Light Limit Fix/Shaders/LightLimitFix/ClusterCullingCS.hlsl
@@ -1,4 +1,4 @@
-#include "LightLimitFix/Common.hlsli"
+#include "Common.hlsli"
 
 cbuffer PerFrame : register(b0)
 {
@@ -9,87 +9,69 @@ cbuffer PerFrame : register(b0)
 //https://github.com/pezcode/Cluster
 
 StructuredBuffer<ClusterAABB> clusters : register(t0);
-StructuredBuffer<Light> lights : register(t1);
+StructuredBuffer<StructuredLight> lights : register(t1);
 
-RWStructuredBuffer<uint> lightIndexCounter : register(u0);
-RWStructuredBuffer<uint> lightIndexList : register(u1);
-RWStructuredBuffer<LightGrid> lightGrid : register(u2);
+RWStructuredBuffer<uint> lightIndexCounter : register(u0);  //1
+RWStructuredBuffer<uint> lightIndexList : register(u1);     //MAX_CLUSTER_LIGHTS * 16^3
+RWStructuredBuffer<LightGrid> lightGrid : register(u2);     //16^3
 
-groupshared Light sharedLights[GROUP_SIZE];
+groupshared StructuredLight sharedLights[GROUP_SIZE];
 
-struct Vector3 {
-	float x, y, z;
-
-	Vector3 operator-(Vector3& other) const {
-		return {x - other.x, y - other.y, z - other.z};
-	} 
-	
-	float dot(const Vector3& other) const {
-		return x * other.x + y * other.y + z * other.z;
-	}
-
-	Vector3 normalize() const {
-		float length = std::sqrt(x * x + y * y + z * z);
-		return {x / length, y / length, z / length};
-	}
-}
-
-bool LightIntersectsCluster(float3 position, float radius, ClusterAABB cluster)
+bool LightIntersectsCluster(StructuredLight light, ClusterAABB cluster, int eyeIndex = 0)
 {
-	float3 closest = max(cluster.minPoint.xyz, min(position, cluster.maxPoint.xyz));
+	float3 closest = max(cluster.minPoint.xyz, min(light.positionVS[eyeIndex].xyz, cluster.maxPoint.xyz));
 
-	float3 dist = closest - position;
-	return dot(dist, dist) <= radius;
+	float3 dist = closest - light.positionVS[eyeIndex].xyz;
+	return dot(dist, dist) <= (light.radius * light.radius);
 }
 
-[numthreads(NUMTHREAD_X, NUMTHREAD_Y, NUMTHREAD_Z)] void main(
-	uint3 groupId
-	: SV_GroupID, uint3 dispatchThreadId
-	: SV_DispatchThreadID, uint3 groupThreadId
-	: SV_GroupThreadID, uint groupIndex
-	: SV_GroupIndex) {
-	if (any(dispatchThreadId >= uint3(CLUSTER_BUILDING_DISPATCH_SIZE_X, CLUSTER_BUILDING_DISPATCH_SIZE_Y, CLUSTER_BUILDING_DISPATCH_SIZE_Z)))
-		return;
+[numthreads(16, 8, 8)] void main(uint3 groupId
+								 : SV_GroupID,
+								 uint3 dispatchThreadId
+								 : SV_DispatchThreadID,
+								 uint3 groupThreadId
+								 : SV_GroupThreadID,
+								 uint groupIndex
+								 : SV_GroupIndex) {
+	if (all(dispatchThreadId == 0)) {
+		lightIndexCounter[0] = 0;
+	}
 
 	uint visibleLightCount = 0;
 	uint visibleLightIndices[MAX_CLUSTER_LIGHTS];
 
-	uint clusterIndex = dispatchThreadId.x +
-	                    dispatchThreadId.y * CLUSTER_BUILDING_DISPATCH_SIZE_X +
-	                    dispatchThreadId.z * (CLUSTER_BUILDING_DISPATCH_SIZE_X * CLUSTER_BUILDING_DISPATCH_SIZE_Y);
+	uint clusterIndex = groupIndex + GROUP_SIZE * groupId.z;
 
 	ClusterAABB cluster = clusters[clusterIndex];
 
-	if (groupIndex < LightCount) {
-		uint lightIndex = groupIndex;
-		Light light = lights[lightIndex];
-		sharedLights[groupIndex] = light;
-	}
+	uint lightOffset = 0;
+	uint lightCount = LightCount;
 
-	GroupMemoryBarrierWithGroupSync();
+	while (lightOffset < lightCount) {
+		uint batchSize = min(GROUP_SIZE, lightCount - lightOffset);
 
-	for (uint i = 0; i < LightCount; i++) {
-		Light light = lights[i];
-
-		float radius = light.radius * light.radius;
-
-#if defined(VR)
-		[branch] if (LightIntersectsCluster(light.positionVS[0].xyz, radius, cluster) || LightIntersectsCluster(light.positionVS[1].xyz, radius, cluster))
-		{
-#else
-		[branch] if (LightIntersectsCluster(light.positionVS[0].xyz, radius, cluster))
-		{
-#endif
-			// The light intersects the cluster area. Check if the light is obstructed.
-			// Draw a line from the light to the center of the cluster and check if it intersects any objects.
-			// If it does, the light is obstructed and should not be rendered within that cluster.
-			// This is a simplified implementation, and a good place to start.
-			
-			visibleLightIndices[visibleLightCount] = i;
-			visibleLightCount++;
-			if (visibleLightCount >= MAX_CLUSTER_LIGHTS)
-				break;
+		if (groupIndex < batchSize) {
+			uint lightIndex = lightOffset + groupIndex;
+			StructuredLight light = lights[lightIndex];
+			sharedLights[groupIndex] = light;
 		}
+
+		GroupMemoryBarrierWithGroupSync();
+
+		for (uint i = 0; i < batchSize; i++) {
+			StructuredLight light = lights[i];
+
+			if (visibleLightCount < MAX_CLUSTER_LIGHTS && (LightIntersectsCluster(light, cluster)
+#ifdef VR
+															  || LightIntersectsCluster(light, cluster, 1)
+#endif  // VR
+																  )) {
+				visibleLightIndices[visibleLightCount] = lightOffset + i;
+				visibleLightCount++;
+			}
+		}
+
+		lightOffset += batchSize;
 	}
 
 	GroupMemoryBarrierWithGroupSync();
@@ -101,11 +83,8 @@ bool LightIntersectsCluster(float3 position, float radius, ClusterAABB cluster)
 		lightIndexList[offset + i] = visibleLightIndices[i];
 	}
 
-	LightGrid output = {
-		offset, visibleLightCount, 0, 0
-	};
-
-	lightGrid[clusterIndex] = output;
+	lightGrid[clusterIndex].offset = offset;
+	lightGrid[clusterIndex].lightCount = visibleLightCount;
 }
 
 //https://www.3dgep.com/forward-plus/#Grid_Frustums_Compute_Shader
@@ -117,22 +96,22 @@ function CullLights( L, C, G, I )
     Input: A list I of global light index list.
     Output: A 2D grid G with the current tiles offset and light count.
     Output: A list I with the current tiles overlapping light indices appended to it.
-
+ 
 1.  let t be the index of the current tile  ; t is the 2D index of the tile.
 2.  let i be a local light index list       ; i is a local light index list.
 3.  let f <- Frustum(t)                     ; f is the frustum for the current tile.
-
+ 
 4.  for l in L                      ; Iterate the lights in the light list.
 5.      if Cull( l, f )             ; Cull the light against the tile frustum.
 6.          AppendLight( l, i )     ; Append the light to the local light index list.
-
-7.  c <- AtomicInc( C, i.count )    ; Atomically increment the current index of the
+                     
+7.  c <- AtomicInc( C, i.count )    ; Atomically increment the current index of the 
                                     ; global light index list by the number of lights
                                     ; overlapping the current tile and store the
                                     ; original index in c.
-
+             
 8.  G(t) <- ( c, i.count )          ; Store the offset and light count in the light grid.
-
-9.  I(c) <- i                       ; Store the local light index list into the global
+         
+9.  I(c) <- i                       ; Store the local light index list into the global 
                                     ; light index list.
 */
