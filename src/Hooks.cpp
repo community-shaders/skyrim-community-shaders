711ed1a8bb84d5ea7309ee96200af5102ea1ba15
Tim <15017472+doodlum@users.noreply.github.com>
2025-07-14 00:35:13 +0100

    docs: more

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -1135,6 +1135,7 @@ namespace Hooks
 		// Patch eye position in BSLightingShader::SetupGeometry to always update due to additional effects which may require it
 		// The variable updateEyePosition is set to false by default. By patching to be true it will always update the eye position
 		// SE and AE use a 6-byte instruction whereas VR uses a 7-byte instruction
+		// We offset from the base address of the containing function to the instruction itself and then to the value the instruction sets
 		{
 			uintptr_t setupGeometryUpdateEyePosition = REL::RelocationID(100565, 107300).address() + REL::Relocate(0x50, 0x75, 0x78);
 			REL::safe_write(setupGeometryUpdateEyePosition + REL::Relocate(6, 6, 7), bool{ true });
