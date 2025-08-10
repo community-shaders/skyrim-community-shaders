171557fd89a27fa642ded40dcfdfc2cc4e9c6bc4
Tim <15017472+doodlum@users.noreply.github.com>
2025-08-10 13:54:12 +0100

    chore: add drs support for llf

--- src/Features/LightLimitFix.cpp
+++ src/Features/LightLimitFix.cpp
@@ -128,7 +128,7 @@ void LightLimitFix::CleanupParticleLights(RE::NiNode* a_node)
 
 void LightLimitFix::SetupResources()
 {
-	auto screenSize = Util::ConvertToDynamic(globals::state->screenSize);
+	auto screenSize = globals::state->screenSize;
 	if (REL::Module::IsVR())
 		screenSize.x *= .5;
 	clusterSize[0] = ((uint)screenSize.x + 63) / 64;
@@ -738,6 +738,13 @@ namespace RE
 
 void LightLimitFix::UpdateLights()
 {
+	auto renderSize = Util::ConvertToDynamic(globals::state->screenSize);
+	if (REL::Module::IsVR())
+		renderSize.x *= .5;
+	clusterSize[0] = ((uint)renderSize.x + 63) / 64;
+	clusterSize[1] = ((uint)renderSize.y + 63) / 64;
+	clusterSize[2] = 32;
+
 	auto smState = globals::game::smState;
 	auto& isl = globals::features::inverseSquareLighting;
 
@@ -964,7 +971,8 @@ void LightLimitFix::UpdateLights()
 		float fov = atan(1.0f / static_cast<float4x4>(projMatrixUnjittered).m[0][0]) * 2.0f * (180.0f / 3.14159265359f);
 
 		static float _lightsNear = 0.0f, _lightsFar = 0.0f, _fov = 0.0f;
-		if (fabs(_fov - fov) > 1e-4 || fabs(_lightsNear - lightsNear) > 1e-4 || fabs(_lightsFar - lightsFar) > 1e-4) {
+		static uint _clusterSizeX = 0, _clusterSizeY = 0, _clusterSizeZ = 0;
+		if (fabs(_fov - fov) > 1e-4 || fabs(_lightsNear - lightsNear) > 1e-4 || fabs(_lightsFar - lightsFar) > 1e-4 || _clusterSizeX != clusterSize[0] || _clusterSizeY != clusterSize[1] || _clusterSizeZ != clusterSize[2]) {
 			LightBuildingCB updateData{};
 			updateData.InvProjMatrix[0] = DirectX::XMMatrixInverse(nullptr, projMatrixUnjittered);
 			if (eyeCount == 1)
@@ -973,6 +981,7 @@ void LightLimitFix::UpdateLights()
 				updateData.InvProjMatrix[1] = DirectX::XMMatrixInverse(nullptr, Util::GetCameraData(1).projMatrixUnjittered);
 			updateData.LightsNear = lightsNear;
 			updateData.LightsFar = lightsFar;
+			std::copy(clusterSize, clusterSize + 3, updateData.ClusterSize);
 
 			lightBuildingCB->Update(updateData);
 
@@ -991,6 +1000,9 @@ void LightLimitFix::UpdateLights()
 			_fov = fov;
 			_lightsNear = lightsNear;
 			_lightsFar = lightsFar;
+			_clusterSizeX = clusterSize[0];
+			_clusterSizeY = clusterSize[1];
+			_clusterSizeZ = clusterSize[2];
 		}
 	}
 
@@ -1005,6 +1017,8 @@ void LightLimitFix::UpdateLights()
 
 		LightCullingCB updateData{};
 		updateData.LightCount = lightCount;
+		std::copy(clusterSize, clusterSize + 3, updateData.ClusterSize);
+
 		lightCullingCB->Update(updateData);
 
 		UINT counterReset[4] = { 0, 0, 0, 0 };
