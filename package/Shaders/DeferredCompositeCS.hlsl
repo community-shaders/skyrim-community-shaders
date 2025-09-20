161d250431f8f9799c18fc58a2ab10c2c4c908f9
doodlum <15017472+doodlum@users.noreply.github.com>
2025-09-20 02:44:44 +0100

    chore: deferred composite missing thing??

--- package/Shaders/DeferredCompositeCS.hlsl
+++ package/Shaders/DeferredCompositeCS.hlsl
@@ -44,6 +44,7 @@ void SampleSSGI(uint2 pixCoord, float3 normalWS, out float ao, out float3 il)
 	ao = 1 - SsgiAoTexture[pixCoord];
 	float4 ssgiIlYSh = SsgiYTexture[pixCoord];
 	float ssgiIlY = max(SphericalHarmonics::SHHallucinateZH3Irradiance(ssgiIlYSh, normalWS), SphericalHarmonics::FuncProductIntegral(ssgiIlYSh, SphericalHarmonics::EvaluateCosineLobe(normalWS)));
+	float2 ssgiIlCoCg = SsgiCoCgTexture[pixCoord];
 	il = max(0, Color::YCoCgToRGB(float3(ssgiIlY, ssgiIlCoCg)));
 }
 
