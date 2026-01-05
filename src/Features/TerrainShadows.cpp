c2987e3402662902730a4bcf7f1281161e3a844e
doodlum <15017472+doodlum@users.noreply.github.com>
2026-01-05 00:27:57 +0000

    fix: fix nonsense

--- src/Features/TerrainShadows.cpp
+++ src/Features/TerrainShadows.cpp
@@ -199,9 +199,9 @@ void TerrainShadows::LoadHeightmap()
 {
 	static auto tes = RE::TES::GetSingleton();
 
-	auto world = tes->GetRuntimeData2().worldSpace;
-	while (world && world->parentWorld && world->parentUseFlags.any(ParentUseFlag::kUseLandData))
-		world = world->parentWorld;
+	auto worldspace = tes->GetRuntimeData2().worldSpace;
+	while (worldspace && worldspace->parentWorld && worldspace->parentUseFlags.any(RE::TESWorldSpace::ParentUseFlag::kUseLandData))
+		worldspace = worldspace->parentWorld;
 
 	if (!worldspace)
 		return;
@@ -314,7 +314,7 @@ void TerrainShadows::UpdateShadow()
 
 	// don't forget to change NTHREADS in shader!
 	constexpr uint updateLength = 1024u;
-	constexpr uint logUpdateLength = std::bit_width(128u) - 1;  // integer log2, https://stackoverflow.com/questions/994593/how-to-do-an-integer-log2-in-c
+	constexpr uint logUpdateLength = std::bit_width(updateLength) - 1;  // integer log2, https://stackoverflow.com/questions/994593/how-to-do-an-integer-log2-in-c
 
 	auto context = globals::d3d::context;
 
