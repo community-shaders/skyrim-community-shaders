31d86665a9d2d9166c2f604f4cab95ffcbf19365
Dawntic <197450198+Dawntic@users.noreply.github.com>
2026-01-10 16:05:41 +1100

    feat: add rasterizer replacement system

--- package/Shaders/ISVolumetricLightingGenerateCS.hlsl
+++ package/Shaders/ISVolumetricLightingGenerateCS.hlsl
@@ -79,7 +79,7 @@ cbuffer PerTechnique : register(b0)
 		{ 0.001, 0.001, 0.001 }
 	};
 
-	float3 normalizedCoordinates = dispatchID.xyz * rcp(TextureDimensions.xyz);
+	float3 normalizedCoordinates = float3(dispatchID.xy + 0.5, dispatchID.z - 1.0) * rcp(TextureDimensions.xyz);
 	float2 uv = normalizedCoordinates.xy;
 	uint eyeIndex = Stereo::GetEyeIndexFromTexCoord(uv);
 	float3 depthUv = Stereo::ConvertFromStereoUV(normalizedCoordinates, eyeIndex) + StepCoefficients[IterationIndex];
---
+++ src/EngineFixes/ShadowmapCascadeRasterizerFix.h
@@ -0,0 +1,46 @@
+#pragma once
+
+// This overrides the shadow cascade rasterizers to fix issues with peter panning and self shadowing
+struct ShadowmapRasterizerFix : EngineFix
+{
+	std::string GetName() override { return "Shadowmap Cascade Rasterizer Fix"; }
+	void Install() override;
+
+	using RasterStateArray = ID3D11RasterizerState* [2][3][12][2];
+
+	static void CloneRasterStates(RasterStateArray* inputArray, int cascade);
+
+	static constexpr uint numCascades = 2;
+
+	static inline RasterStateArray* gRasterStates = nullptr;
+	static inline RasterStateArray backupGameRasterStates = {};
+	static inline RasterStateArray shadowmapRasterStates[numCascades] = {};
+
+	static constexpr int firstCascadeDepthBias = 160;
+	static constexpr float firstCascadeDepthBiasClamp = 0.004f;
+	static constexpr float firstCascadeSlopeScaleBias = 3.2f;
+
+	static constexpr int secondCascadeDepthBias = 0;
+	static constexpr float secondCascadeDepthBiasClamp = 0.005f;
+	static constexpr float secondCascadeSlopeScaleBias = 3.8f;
+
+	struct ShadowMapRasterizerDescriptor
+	{
+		int rasterDepthBias;
+		float rasterDepthBiasClamp;
+		float rasterSlopeScaleBias;
+	};
+	static void GetUpdatedRasterDesc(D3D11_RASTERIZER_DESC& outputDesc, ShadowMapRasterizerDescriptor desc);
+
+	static constexpr ShadowMapRasterizerDescriptor cascadeDescriptors[numCascades] = {
+		{ 0, firstCascadeDepthBiasClamp, firstCascadeSlopeScaleBias },
+		{ 0, secondCascadeDepthBiasClamp, secondCascadeSlopeScaleBias }
+	};
+
+	struct BSShadowDirectionalLight_RenderShadowmaps_RenderCascade
+	{
+		static void thunk(RE::BSShadowDirectionalLight* light, void* arg1, void* arg2, uint32_t flags);
+		static inline REL::Relocation<decltype(thunk)> func;
+	};
+};
+#pragma once
