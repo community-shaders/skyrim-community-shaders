ab8caa0fac1bdbfd66f1332f2ea40839bcb95088
Jiaye <l936249247@hotmail.com>
2026-01-16 12:03:12 +0800

    fix(water): disable water history blend

--- package/Shaders/ISWaterBlend.hlsl
+++ package/Shaders/ISWaterBlend.hlsl
@@ -7,20 +7,26 @@ typedef VS_OUTPUT PS_INPUT;
 struct PS_OUTPUT
 {
 	float3 Color : SV_Target0;
+#if defined(WATER_HISTORY_BLEND)
 	float4 Color1 : SV_Target1;
+#endif
 };
 
 #if defined(PSHADER)
 SamplerState sourceSampler : register(s0);
+#if defined(WATER_HISTORY_BLEND)
 SamplerState waterHistorySampler : register(s1);
 SamplerState motionBufferSampler : register(s2);
 SamplerState depthBufferSampler : register(s3);
+#endif
 SamplerState waterMaskSampler : register(s4);
 
 Texture2D<float4> sourceTex : register(t0);
+#if defined(WATER_HISTORY_BLEND)
 Texture2D<float4> waterHistoryTex : register(t1);
 Texture2D<float4> motionBufferTex : register(t2);
 Texture2D<float4> depthBufferTex : register(t3);
+#endif
 Texture2D<float4> waterMaskTex : register(t4);
 
 cbuffer PerGeometry : register(b2)
@@ -28,6 +34,7 @@ cbuffer PerGeometry : register(b2)
 	float4 NearFar_Menu_DistanceFactor : packoffset(c0);
 };
 
+#if defined(WATER_HISTORY_BLEND)
 float3 LogToLinear(float3 logColor)
 {
     const float linearRange = 14.0f;
@@ -43,6 +50,7 @@ float3 LinearToLog(float3 linearColor)
     const float exposureGrey = 444.0f;
     return saturate(log2(linearColor) / linearRange - log2(linearGrey) / linearRange + exposureGrey / 1023.0f);
 }
+#endif
 
 PS_OUTPUT main(PS_INPUT input)
 {
@@ -55,6 +63,7 @@ PS_OUTPUT main(PS_INPUT input)
 	}
 
 	float3 sourceColor = sourceTex.Sample(sourceSampler, adjustedScreenPosition).xyz;
+#if defined(WATER_HISTORY_BLEND)
 	float2 motion = motionBufferTex.Sample(motionBufferSampler, adjustedScreenPosition).xy;
 	float2 motionScreenPosition = Stereo::ConvertToStereoUV(Stereo::ConvertFromStereoUV(input.TexCoord, eyeIndex) + motion, eyeIndex);
 	float2 motionAdjustedScreenPosition =
@@ -62,8 +71,10 @@ PS_OUTPUT main(PS_INPUT input)
 	float4 waterHistory =
 		waterHistoryTex.Sample(waterHistorySampler, motionAdjustedScreenPosition).xyzw;
 	waterHistory.xyz = LogToLinear(waterHistory.xyz) - LogToLinear(0);
+#endif
 
 	float3 finalColor = sourceColor;
+#if defined(WATER_HISTORY_BLEND)
 	if (
 #	ifndef VR
 		motionScreenPosition.x >= 0 && motionScreenPosition.y >= 0 && motionScreenPosition.x <= 1 &&
@@ -85,6 +96,7 @@ PS_OUTPUT main(PS_INPUT input)
 	}
 
 	psout.Color1 = float4(LinearToLog(finalColor + LogToLinear(0)), 1);
+#endif
 	psout.Color = finalColor;
 
 	return psout;
