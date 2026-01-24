8af538295cdb879d75f8ed79d17b33adcc3e65a0
doodlum <15017472+doodlum@users.noreply.github.com>
2026-01-24 00:57:57 +0000

    chore: make rabbit happy

--- features/Skylighting/Shaders/Skylighting/UpdateProbesCS.hlsl
+++ features/Skylighting/Shaders/Skylighting/UpdateProbesCS.hlsl
@@ -154,8 +154,8 @@ float Get2DFilteredShadow(float3 positionWS, uint eyeIndex, out bool validShadow
 		float3(-0.469, 0.381, 0.947)
 	};
 
-	uint shadowVisibilityBitShift = outShadowVisibilityBitShiftArray[dtid];
-
+	uint shadowVisibilityBitShift = isValid ? outShadowVisibilityBitShiftArray[dtid] : 0;
+	
 	cellCentreMS += noise3D[shadowVisibilityBitShift] * Skylighting::CELL_SIZE;
 
 	float3 viewDirection = FrameBuffer::WorldToView(-normalize(cellCentreMS), false);
