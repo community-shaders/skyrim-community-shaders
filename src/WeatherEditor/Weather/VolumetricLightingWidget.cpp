4cfdcefea2ceb9db62eb38f6d57349a4c998b5b6
SkrubbySkrubInAShrub <skrubbyskrubinashrub@gmail.com>
2026-02-08 23:48:37 +0100

    more slider edits.

--- src/WeatherEditor/Weather/VolumetricLightingWidget.cpp
+++ src/WeatherEditor/Weather/VolumetricLightingWidget.cpp
@@ -50,7 +50,7 @@ void VolumetricLightingWidget::DrawWidget()
 				ImGui::SeparatorText("Phase Function");
 				if (WeatherUtils::DrawSliderFloat("Contribution", settings.phaseFunctionContribution, 0.0f, 1.0f))
 					changed = true;
-				if (WeatherUtils::DrawSliderFloat("Scattering", settings.phaseFunctionScattering, -1.0f, 1.0f))
+				if (WeatherUtils::DrawSliderFloat("Scattering", settings.phaseFunctionScattering, 0.0f, 1.0f))
 					changed = true;
 
 				ImGui::SeparatorText("Sampling");
