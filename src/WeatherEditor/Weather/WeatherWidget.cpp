99e61fa741a44f2dd370c55a37e00f49f7160fd1
SkrubbySkrubInAShrub <skrubbyskrubinashrub@gmail.com>
2026-02-12 14:01:45 +0100

    fix(WeatherWidget): cleanup

--- src/WeatherEditor/Weather/WeatherWidget.cpp
+++ src/WeatherEditor/Weather/WeatherWidget.cpp
@@ -465,8 +465,8 @@ void WeatherWidget::LoadSettings()
 	}
 	InitializeInheritFlags();
 	LoadFeatureSettings();
-	ApplyChanges();
 	originalSettings = settings;
+	ApplyChanges();
 }
 
 void WeatherWidget::SaveSettings()
@@ -1563,67 +1563,10 @@ void WeatherWidget::RevertChanges()
 	ApplyChanges();
 }
 
-static bool IsDefaultFeatureOverrides(const json& value)
-{
-	if (!value.is_object())
-		return true;
-	if (value.empty())
-		return true;
-	if (value.size() == 1 && value.contains("__enabled"))
-		return !value.value("__enabled", false);
-	return false;
-}
-
-static bool AreFlagsEqualNormalized(const std::map<std::string, bool>& a, const std::map<std::string, bool>& b)
-{
-	for (const auto& [key, value] : a) {
-		auto it = b.find(key);
-		if (it == b.end()) {
-			if (value)
-				return false;
-			continue;
-		}
-		if (value != it->second)
-			return false;
-	}
-
-	for (const auto& [key, value] : b) {
-		if (value && a.find(key) == a.end())
-			return false;
-	}
-
-	return true;
-}
-
-static bool AreFeatureSettingsEqualNormalized(const std::map<std::string, json>& a, const std::map<std::string, json>& b)
-{
-	for (const auto& [key, value] : a) {
-		const bool aDefault = IsDefaultFeatureOverrides(value);
-		auto it = b.find(key);
-		if (it == b.end()) {
-			if (!aDefault)
-				return false;
-			continue;
-		}
-		const bool bDefault = IsDefaultFeatureOverrides(it->second);
-		if (aDefault != bDefault)
-			return false;
-		if (!aDefault && value != it->second)
-			return false;
-	}
-
-	for (const auto& [key, value] : b) {
-		if (!IsDefaultFeatureOverrides(value) && a.find(key) == a.end())
-			return false;
-	}
-
-	return true;
-}
-
 bool WeatherWidget::Settings::operator==(const Settings& o) const
 {
 	return parent == o.parent &&
-	       AreFlagsEqualNormalized(inheritFlags, o.inheritFlags) &&
+	       inheritFlags == o.inheritFlags &&
 	       weatherProperties == o.weatherProperties &&
 	       weatherColors == o.weatherColors &&
 	       fogProperties == o.fogProperties &&
@@ -1631,7 +1574,7 @@ bool WeatherWidget::Settings::operator==(const Settings& o) const
 	       std::equal(std::begin(dalc), std::end(dalc), std::begin(o.dalc)) &&
 	       std::equal(std::begin(clouds), std::end(clouds), std::begin(o.clouds)) &&
 	       std::equal(std::begin(imageSpaces), std::end(imageSpaces), std::begin(o.imageSpaces)) &&
-	       AreFeatureSettingsEqualNormalized(featureSettings, o.featureSettings);
+	       featureSettings == o.featureSettings;
 }
 
 bool WeatherWidget::HasUnsavedChanges() const
