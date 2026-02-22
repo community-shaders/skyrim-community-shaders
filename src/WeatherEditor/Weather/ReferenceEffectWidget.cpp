908489839b105c657f28049d3b8e26692dd8bf21
SkrubbySkrubInAShrub <skrubbyskrubinashrub@gmail.com>
2026-02-22 16:45:09 +0100

    fix:nitpick

--- src/WeatherEditor/Weather/ReferenceEffectWidget.cpp
+++ src/WeatherEditor/Weather/ReferenceEffectWidget.cpp
@@ -38,6 +38,7 @@ void ReferenceEffectWidget::DrawWidget()
 				changed = true;
 
 			if (changed && editorWindow->settings.autoApplyChanges) {
+				editorWindow->PushUndoState(this);
 				ApplyChanges();
 			}
 		}
