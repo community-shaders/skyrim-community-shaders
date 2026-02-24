8a94e4dd1db53209f00fa81871455fb270c06626
doodlum <15017472+doodlum@users.noreply.github.com>
2026-02-24 23:23:05 +0000

    feat: other shadow types

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2634,7 +2634,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 		float shadowComponent = 1.0;
 		if (Permutation::PixelShaderDescriptor & Permutation::LightingFlags::DefShadow) {
 			if (light.lightFlags & LightLimitFix::LightFlags::Shadow) {
-				//shadowComponent = ShadowSampling::GetShadowLightShadow(light.shadowLightIndex, input.WorldPosition.xyz);
+				shadowComponent = ShadowSampling::GetShadowLightShadow(light.shadowLightIndex, input.WorldPosition.xyz, rotationMatrix);
 				lightShadow *= shadowComponent;
 			}
 		}
