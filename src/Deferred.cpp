32d1813c7c1a52c3d49280ea270987c0c2ef3682
doodlum <15017472+doodlum@users.noreply.github.com>
2026-03-07 21:49:52 +0000

    fix: fix nonsense

--- src/Deferred.cpp
+++ src/Deferred.cpp
@@ -577,7 +577,7 @@ void Deferred::SetShadowCascadeParameters(RE::BSShadowLight::RUNTIME_DATA& light
 		DirectX::XMStoreFloat4x4(&dd.ShadowProj[i], proj);
 
 		DirectX::XMMATRIX invProj = DirectX::XMMatrixTranspose(proj);
-		DirectX::XMStoreFloat4x4(&dd.InvShadowProj[i], proj);
+		DirectX::XMStoreFloat4x4(&dd.InvShadowProj[i], invProj);
 	}
 }
 
@@ -588,7 +588,7 @@ void Deferred::SetShadowCascadeParameters(RE::BSShadowLight::RUNTIME_DATA_VR& li
 		DirectX::XMStoreFloat4x4(&dd.ShadowProj[i], proj);
 
 		DirectX::XMMATRIX invProj = DirectX::XMMatrixTranspose(proj);
-		DirectX::XMStoreFloat4x4(&dd.InvShadowProj[i], proj);
+		DirectX::XMStoreFloat4x4(&dd.InvShadowProj[i], invProj);
 	}
 }
 
@@ -599,7 +599,7 @@ void Deferred::SetShadowParameters(RE::BSShadowLight::RUNTIME_DATA& lightData, S
 	DirectX::XMStoreFloat4x4(&sd.ShadowProj, proj);
 
 	DirectX::XMMATRIX invProj = DirectX::XMMatrixTranspose(proj);
-	DirectX::XMStoreFloat4x4(&sd.InvShadowProj, proj);
+	DirectX::XMStoreFloat4x4(&sd.InvShadowProj, invProj);
 }
 
 void Deferred::SetShadowParameters(RE::BSShadowLight::RUNTIME_DATA_VR& lightData, ShadowData& sd)
@@ -609,7 +609,7 @@ void Deferred::SetShadowParameters(RE::BSShadowLight::RUNTIME_DATA_VR& lightData
 	DirectX::XMStoreFloat4x4(&sd.ShadowProj, proj);
 
 	DirectX::XMMATRIX invProj = DirectX::XMMatrixTranspose(proj);
-	DirectX::XMStoreFloat4x4(&sd.InvShadowProj, proj);
+	DirectX::XMStoreFloat4x4(&sd.InvShadowProj, invProj);
 }
 
 void Deferred::CopyShadowData()
