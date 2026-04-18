2bc1445470e31996ecac7cf3b3a7e26db6190258
doodlum <15017472+doodlum@users.noreply.github.com>
2026-04-18 15:52:10 +0100

    chore: dont touch ibl i guess

--- src/Features/IBL.cpp
+++ src/Features/IBL.cpp
@@ -202,10 +202,10 @@ IBL::Settings IBL::GetCommonBufferData() const
 			if (settingManager.GetValue<bool>("EnableImageBasedLighting", "EFFECT")) {
 				data.EnableIBL = !Util::IsInterior();
 				data.EnvIBLScale = 0.0f;
-				data.SkyIBLScale = settingManager.GetInterpolatedTimeOfDayValue("MultiplicativeAmount", "IMAGEBASEDLIGHTING") / 2.0f;
+				data.SkyIBLScale = settingManager.GetInterpolatedTimeOfDayValue("MultiplicativeAmount", "IMAGEBASEDLIGHTING");
 				data.DALCAmount = 1.0f;
-				data.EnvIBLSaturation = 2.5f;
-				data.SkyIBLSaturation = 2.5f;
+				data.EnvIBLSaturation = 2.0f;
+				data.SkyIBLSaturation = 2.0f;
 				data.DALCMode = 3;
 			}
 		}
