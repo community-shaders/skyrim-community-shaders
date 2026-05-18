a4748afa055c4a5ef0e32655de58faa44badad35
doodlum <15017472+doodlum@users.noreply.github.com>
2026-05-18 14:03:04 +0100

    chore: more aggressive dither

--- features/Effect11/Shaders/Effect11/CopyPS.hlsl
+++ features/Effect11/Shaders/Effect11/CopyPS.hlsl
@@ -15,7 +15,7 @@ float3 ScreenSpaceDither(float2 vScreenPos)
 {
 	float3 vDither = dot(float2(171.0, 231.0), vScreenPos.xy + Timer).xxx;
 	vDither.rgb = frac(vDither.rgb / float3(103.0, 71.0, 97.0)) - float3(0.5, 0.5, 0.5);
-	return (vDither.rgb / 255.0) * 0.375;
+	return vDither.rgb / 255.0;
 }
 
 float4 main(PS_INPUT input) : SV_TARGET
