b01a977c0eaecb735f25c81a5f5dc2b8c1d1d5f8
doodlum <15017472+doodlum@users.noreply.github.com>
2026-06-10 23:16:05 +0100

    fix: vsm precision fixes

--- src/Features/VolumetricShadows.cpp
+++ src/Features/VolumetricShadows.cpp
@@ -101,7 +101,7 @@ void VolumetricShadows::CopyShadowLightData()
 				copyDesc.Height = SHADOW_COPY_SIZE;
 				copyDesc.MipLevels = 2;
 				copyDesc.ArraySize = 1;
-				copyDesc.Format = DXGI_FORMAT_R16G16_UNORM;
+				copyDesc.Format = DXGI_FORMAT_R32G32_UNORM;
 				copyDesc.SampleDesc.Count = 1;
 				copyDesc.SampleDesc.Quality = 0;
 				copyDesc.Usage = D3D11_USAGE_DEFAULT;
